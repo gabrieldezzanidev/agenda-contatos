99da308b1885c888becc307df6f485b8668a0f68
Gabriel Dezzani Pitzer <gabrielpitzer@hotmail.com>
2024-05-08 14:54:24 -0300

    termino js

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 const form = document.getElementById('form-agenda');
+const contatos = [];
+const numeros = [];
 
 let linhas = '';
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    
+
     adicionaLinha();
     atualizaTabela();
 })
 
-
 function adicionaLinha() {
-    const inputNomeContato = document.getElementById('nome-contato')
-    const inputNumeroContato  = document.getElementById('numero-contato')
-}
+    const inputNomeContato = document.getElementById ('nome-contato');
+    const inputNumeroContato = document.getElementById ('numero-contato');
+    
+    
+    contatos.push(inputNomeContato.value);
+    numeros.push(inputNumeroContato.value);
 
-let linha = '<tr>';
-    linha += `<td>${inputNomeContato.value}</td>`;
-    linha += `<td>${inputNumeroContato.value}</td>`;
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}`;
+    linha += `<td>${inputNumeroContato.value}`;
+    linha += '</tr>';
 
     linhas += linha
 
-    function atualizaTabela(){
-        const corpoTabela = document.querySelector('tbody'); 
-        corpoTabela.innerHTML = linhas;
-    }   
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela () {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
